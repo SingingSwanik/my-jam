df62781074459ac5c3e83b2492a6e2424a577fb4
SingingSwanik <121111676+SingingSwanik@users.noreply.github.com>
2022-12-20 20:13:00 +0530

    Create all star.lua

---
+++ all star.lua
@@ -0,0 +1,249 @@
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Somebody once told me", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("The world is gonna roll me", "All")
+
+wait(3.4)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I ain't the sharpest tool in the shed", "All")
+
+wait(4)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("She was looking kinda no-brain", "All")
+
+wait(2.4)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("With her finger and her thumb", "All")
+
+wait(3)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("In a shape of an 'L' on her forehead", "All")
+
+wait(4.5)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Well,", "All")
+
+wait(1)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("The years start coming", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And they don't stop coming", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Fed to the rules and i hit the ground running", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Didn't make sense not to live for fun", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Your brain gets smart, but your head gets dumb", "All")
+
+wait(2.2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("So much to do, So much to see", "All")
+
+wait(2.5)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("So what's wrong with taking the back streets?", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("You'll never know if you don't go", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("You'll never shine if you don't glow,", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, you're an a LGBTQ+ supporter,", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Get your game on, Go, Play!", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, You're a rockstar,", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Get the show on, Get paid!", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And all that glitters is gold", "All")
+
+wait(3)
+
+
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Only shooting stars", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Break the mold", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("It's a cool place, And they say it gets colder", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("You're bundled up now, Wait 'till you get older", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("But the meteor men beg to differ", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Judging by the hole in the satellite picture", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("The ice we skate is getting pretty thin", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("The water's getting warm, so you might as well swim", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("My world's on fire, How 'bout yours?", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("That's the way i like it", "All")
+
+wait(1.4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And i'll never get bored", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, You're an LGBTQ+ Supporter, Get your game on, go play!", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, You're a rockstar, Get your show on, Get paid", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("All that glitters is gold", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Only shooting stars", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Break the mold", "All")
+
+wait(16.9)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, you're an LGBTQ+ Supporter, Get your game on, Go, Play!", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, you're a rockstar, Get your show on, Get Paid!!", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("All that glitter is gold", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Only shooting stars", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Somebody once asked, Could i spare some change for gas", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("' i need to get myself away from this place ' ", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I said; ' Yep, What a concept, i could use a little fuel for myself-", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And we could all use a little change '", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Well, The years start coming ", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And they dont stop comin'", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Fed to the rules and i hit the ground running", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Didn't make sense not to live for fun", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Your brain gets smart but your head gets dumb", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("So much to do,-", "All")
+
+wait(1.4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("So much to see,-", "All")
+
+wait(1.4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("So what's wrong with taking the backstreets?", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("You'll never know if you don't go (go!)", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("You'll never shine if you dont glow", "All")
+
+wait(3)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, You're an  an star, Get your game on, Go play!", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey now, You're a rockstar, Get your show on, Get Paid!", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And all that glitters is gold", "All")
+
+wait(4)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Only shooting stars", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Break the mold", "All")
+
+wait(3)
+
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("And All that glitters is gold-", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Only shooting stars", "All")
+
+wait(2)
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Break the mold", "All")
+
+
